011128aa9010f9d6ada4cd3bdad6b5c9ed097163
Zach McManus <59886732+zachmdsi@users.noreply.github.com>
2025-07-06 15:36:19 -0500

    Run isort to fix import order

--- src/kc_fep_poc/__init__.py
+++ src/kc_fep_poc/__init__.py
@@ -1,8 +1,8 @@
 """Kolmogorov-complexity and Free-Energy toy module."""
 
 from .dataset import write_dataset
-from .metrics import Metrics, compute_metrics, generate_observations
 from .free_energy import free_energy_step
+from .metrics import Metrics, compute_metrics, generate_observations
 
 __all__ = [
     "Metrics",
